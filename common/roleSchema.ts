00cebe08d4ad7cfe33be74e47f0f4c6555effd72
Alaa Rihan <alaa.addeen@gmail.com>
2021-06-16 21:10:10 +0300

    Fix

--- common/roleSchema.ts
+++ common/roleSchema.ts
@@ -1,10 +1,13 @@
 import { GraphQLSchema } from 'graphql'
 import NodeCache from 'node-cache'
 import { getRoleSchema } from '../roleSchema'
-export const roleSchemaCache = new NodeCache()
+export const roleSchemaCache = new NodeCache({useClones: false})
 
-export async function getRoleSchemaCache(schema: GraphQLSchema, role): Promise<GraphQLSchema> {
-  if (roleSchemaCache.get(role) === undefined) {
+export async function getRoleSchemaCache(
+  schema: GraphQLSchema,
+  role,
+): Promise<GraphQLSchema> {
+  if (!(roleSchemaCache.get(role) instanceof GraphQLSchema)) {
     const roleSchema = await getRoleSchema(schema, role)
     roleSchemaCache.set(role, roleSchema)
   }
