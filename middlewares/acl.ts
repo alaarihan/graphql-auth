bf0a486fcee48dee0d1472a0ae332988df49ec99
Alaa Rihan <alaa.addeen@gmail.com>
2021-06-26 07:29:26 +0300

    Undo

--- middlewares/acl.ts
+++ middlewares/acl.ts
@@ -668,7 +668,7 @@ function uniqueWhereToManyWhere(where) {
 }
 
 export function mergeCheckWithWhere(where, check) {
-  if (where === undefined) return check ? check : where
+  if (where === undefined) return check
   if (where.AND) {
     where.AND.push(check)
   } else {
